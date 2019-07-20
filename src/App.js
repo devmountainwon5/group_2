a3ff8095b339631d4875475927c8db9f8f54c4de
Ali's Macbook <Ali@alis-mbp.attlocal.net>
2019-07-20 13:30:22 -0500

    authO

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className='App'>
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path='/' exact />
+          <Route path='/profile' component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
