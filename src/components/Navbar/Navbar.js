26b8b3227c31ba5cb7868de3098b46993bc4df3f
F15HYF4C3 <t.nicole.education@gmail.com>
2019-07-22 21:28:36 -0600

    NavBar Styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { useAuth0 } from './../../react-auth0-wrapper';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import { useAuth0 } from "./../../react-auth0-wrapper";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+// import { spacing } from "@material-ui/system";
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  return (
-    <div>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
-      )}
+	return (
+		<div className="nav">
+			<span>
+				{!isAuthenticated && (
+					<button onClick={() => loginWithRedirect({})}>Log in</button>
+				)}
 
-      {isAuthenticated && (
-        <span>
-          <Link to='/'>Home</Link>&nbsp;
-          <Link to='/profile'>Profile</Link>&nbsp;
-          {/* NEW - Add a link to the /external-api route */}
-          <Link to='/external-api'>External API</Link>
-        </span>
-      )}
+				{isAuthenticated && (
+					<span>
+						<Button variant="contained" color="primary" className="nav-buttons">
+							<Link to="/">Home</Link>&nbsp;
+						</Button>
+						{/* <spacing m="2rem" /> */}
+						<Button variant="contained" color="primary" className="nav-buttons">
+							<Link to="/profile">Profile</Link>
+							&nbsp;
+						</Button>
 
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-    </div>
-  );
+						{/* NEW - Add a link to the /external-api route */}
+						{/* <Link to="/external-api">External API</Link> */}
+					</span>
+				)}
+
+				{isAuthenticated && (
+					<Button
+						onClick={() => logout()}
+						variant="contained"
+						color="primary"
+						className="nav-buttons">
+						Log out
+					</Button>
+				)}
+			</span>
+		</div>
+	);
 };
 
 export default NavBar;
