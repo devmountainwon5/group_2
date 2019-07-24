c902f2014aafcfe8a3201b4d57ca1febb39d293b
Tyra Muhlestein <tmuhlestein@xpressbillpay.net>
2019-07-23 21:14:27 -0600

    MaterialUI Themed NavBar - Waiting for Team Suggestions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import NavBar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Profile from './components/Profile/Profile';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import ExternalApi from './components/ExternalApi/ExternalApi';
+import React from "react";
+import NavBar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile/Profile";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import ExternalApi from "./components/ExternalApi/ExternalApi";
 
 function App() {
-  return (
-    <div className='App'>
-      {/* New - use BrowserRouter to provide access to /profile */}
-      <BrowserRouter>
-        <header>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route path='/' exact />
-          <PrivateRoute path='/profile' component={Profile} />
-          <PrivateRoute path='/external-api' component={ExternalApi} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* New - use BrowserRouter to provide access to /profile */}
+			<BrowserRouter>
+				<header>
+					<NavBar />
+				</header>
+				<Switch>
+					<Route path="/" exact />
+					<PrivateRoute path="/profile" component={Profile} />
+					<PrivateRoute path="/external-api" component={ExternalApi} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,46 +2,110 @@ import React from "react";
 import "./Navbar.css";
 import { useAuth0 } from "./../../react-auth0-wrapper";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-// import { spacing } from "@material-ui/system";
+
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+//color pallete
+
+//Navy Slate: #002642
+//Black: #02040f
+//Yellow-Orange: #e59500
+//Light-Grey: #e5dada
+//Red: #81171b
+
+const colors = createMuiTheme({
+	palette: {
+		primary: {
+			light: "#e59500",
+			main: "#002642",
+			dark: "#02040f",
+			contrastText: "#81171b"
+		},
+		secondary: {
+			light: "#e5dada",
+			main: "#e59500",
+			dark: "#81171b",
+			contrastText: "#002642"
+		}
+	}
+});
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		background: "linear-gradient(45deg, #002642 30%, #e5dada 90%)",
+		border: 0,
+		borderRadius: 3,
+		boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+		color: "e5dada",
+		height: 48,
+		padding: "0 30px",
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	title: {
+		flexGrow: 1
+	}
+}));
 
 const NavBar = () => {
 	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+	const classes = useStyles();
 
 	return (
-		<div className="nav">
-			<span>
-				{!isAuthenticated && (
-					<button onClick={() => loginWithRedirect({})}>Log in</button>
-				)}
+		<ThemeProvider className={classes.root} theme={colors}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6" className={classes.title}>
+						{!isAuthenticated && (
+							<Button color="secondary" onClick={() => loginWithRedirect({})}>
+								Log in
+							</Button>
+						)}
 
-				{isAuthenticated && (
-					<span>
-						<Button variant="contained" color="primary" className="nav-buttons">
-							<Link to="/">Home</Link>&nbsp;
-						</Button>
-						{/* <spacing m="2rem" /> */}
-						<Button variant="contained" color="primary" className="nav-buttons">
-							<Link to="/profile">Profile</Link>
-							&nbsp;
-						</Button>
+						{isAuthenticated && (
+							<span>
+								<Button
+									variant="contained"
+									color="secondary"
+									className="nav-buttons">
+									<Link to="/">Home</Link>&nbsp;
+								</Button>
+								{/* <spacing m="2rem" /> */}
 
-						{/* NEW - Add a link to the /external-api route */}
-						{/* <Link to="/external-api">External API</Link> */}
-					</span>
-				)}
+								<Button
+									variant="contained"
+									color="secondary"
+									className="nav-buttons">
+									<Link to="/profile">Profile</Link>
+									&nbsp;
+								</Button>
 
-				{isAuthenticated && (
-					<Button
-						onClick={() => logout()}
-						variant="contained"
-						color="primary"
-						className="nav-buttons">
-						Log out
-					</Button>
-				)}
-			</span>
-		</div>
+								{/* NEW - Add a link to the /external-api route */}
+								{/* <Link to="/external-api">External API</Link> */}
+							</span>
+						)}
+
+						{isAuthenticated && (
+							<Button
+								onClick={() => logout()}
+								variant="contained"
+								color="secondary"
+								className="nav-buttons">
+								Log out
+							</Button>
+						)}
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</ThemeProvider>
 	);
 };
 
