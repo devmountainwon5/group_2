3a1d47e27d6617fd92eeb3489d12e3d2915ae245
Sydney <sjbarney99@gmail.com>
2019-08-07 19:59:08 -0600

    Initial commit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,39 +1,33 @@
-import React from 'react';
-import { useAuth0 } from './../../react-auth0-wrapper';
+import React from "react";
+import { useAuth0 } from "./../../react-auth0-wrapper";
 import Favorites from "../Favorites/Favorites";
-import "./Profile.css"
-
+import "./Profile.css";
 
 const Profile = () => {
 	const { loading, user } = useAuth0();
-	// componentDidMount(){
-	//   console.log
-	// }
+
 	if (loading || !user) {
 		return "Loading...";
 	}
 
+	// return (
+	// 	<>
+	// 		<img src={user.picture} alt='Profile' />
+
 	return (
-		<>
-			<img src={user.picture} alt='Profile' />
+		<div>
+			<div className='profile'>
+				<img src={user.picture} alt='Profile' />
 
-  return (
-    <div>
-      <div className='profile'>
-      <img src={user.picture} alt='Profile'/>
+				<h2>{user.name}</h2>
+				<p>{user.email}</p>
+			</div>
 
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      </div>
-
-      <Favorites/>
-      <Favorites/>
-      <Favorites/>
-      
-    </div>
-    
-
-  );
+			<Favorites />
+			<Favorites />
+			<Favorites />
+		</div>
+	);
 };
 
-export default connect(state => state)(Profile);
+export default Profile;
