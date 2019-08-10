fe0fe233e4ecfa42477cbab1d7eb2b1d75e51b1b
Sydney <sjbarney99@gmail.com>
2019-08-10 12:19:25 -0600

    single page link to google maps

--- src/components/Home/RestaurantList/ListItem.js
+++ src/components/Home/RestaurantList/ListItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../Home.css";
-import SingleView from "./src/SingleView/SingleView";
+// import { Link } from "react-router-dom";
+// import SingleView from "./src/components/Home/SingleView/SingleView";
 
 export default function List(props) {
 	return (
 		<div className='row'>
 			<div className='card-deck'>
 				<div className='card'>
-					<SingleView />
+					{/* <SingleView /> */}
 					{/* <Link to={`/posts/${props.id}`}> */}
 					<img src={props.picture} className='card-img-top' />
 					<div className='card-body'>
@@ -15,6 +16,14 @@ export default function List(props) {
 						<h4>{props.rating}</h4>
 						<h5>{props.address}</h5>
 						<button className='btn btn-primary'>ADD</button>
+						<a
+							target='blank'
+							href={
+								"https://www.google.com/maps/search/?api=1&query=Google&query_place_id=" +
+								props.place_id
+							}>
+							<button className='google review'>More Info</button>
+						</a>
 					</div>
 					{/* </Link> */}
 				</div>
--- src/components/Home/SingleView/SingleView.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SingleView(props) {
-	return <div>{props.restaraunt}</div>;
-}
