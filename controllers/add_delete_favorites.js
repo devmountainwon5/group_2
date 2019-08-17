8420f224be117bfd4a7e07ebf725e8f65658b556
Ali's Macbook <Ali@alis-mbp.attlocal.net>
2019-08-17 12:38:38 -0500

    add button function is working

--- controllers/add_delete_favorites.js
+++ controllers/add_delete_favorites.js
@@ -1,78 +1,89 @@
 module.exports = {
-   addFavorite: (req, res, next) => {
-       const dbInstance = req.app.get('db');
+  addFavorite: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
-       const { res_name, res_address, place_id, res_pic, userEmail } = req.body;
-       dbInstance.get_user([userEmail])
-       .then( results => {
-           let user_id = results[0].id;
+    const { res_name, res_address, place_id, res_pic } = req.body;
+    const { userEmail } = req.session;
+    if (userEmail) {
+      dbInstance
+        .get_user([userEmail])
+        .then(results => {
+          let user_id = results[0].id;
 
-           dbInstance.check_favorite_exists([place_id])
-            .then( results => {
-                if (!results[0]) {    
-                    dbInstance.add_favorite([res_name, res_address, res_pic, place_id])
-                    .then( results => {
-                        dbInstance.add_to_user([user_id, results[0].favorite_id])
-                        .then( results => {
-                            res.status(200).send(results)
-                        })
-                        .catch( err => {
-                            res.status(500).send(err)
-                        })
-                    })
-                    .catch( err => {        
-                        res.status(500).send(err)
-                    })
-                } else {
-                     dbInstance.add_to_user([user_id, results[0].favorite_id])
-                        .then( results => {
-                            res.status(200).send(results)
-                        })
-                        .catch( err => {
-                            res.status(500).send(err)
-                        })
-                };
+          dbInstance
+            .check_favorite_exists([place_id])
+            .then(results => {
+              if (!results[0]) {
+                dbInstance
+                  .add_favorite([res_name, res_address, res_pic, place_id])
+                  .then(results => {
+                    dbInstance
+                      .add_to_user([user_id, results[0].favorite_id])
+                      .then(results => {
+                        res.status(200).send(results);
+                      })
+                      .catch(err => {
+                        res.status(500).send(err);
+                      });
+                  })
+                  .catch(err => {
+                    res.status(500).send(err);
+                  });
+              } else {
+                dbInstance
+                  .add_to_user([user_id, results[0].favorite_id])
+                  .then(results => {
+                    res.status(200).send(results);
+                  })
+                  .catch(err => {
+                    res.status(500).send(err);
+                  });
+              }
             })
-            .catch( err => {
-                res.status(500).send(err)
+            .catch(err => {
+              res.status(500).send(err);
             });
-       })
-       .catch( err => {
-           res.status(500).send(err);
-       })
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    } else {
+      res.status(200).send('Please Sign In');
+    }
+  },
+  deleteFavorite: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
-       
-   },
-   deleteFavorite: (req, res, next) => {
-       const dbInstance = req.app.get('db');
+    const { favorite_id } = req.params;
 
-       const { favorite_id } = req.params;
-
-       dbInstance.delete_favorite([favorite_id])
-       .then( results => {
-           res.status(200).send('Favorite has been removed')
-       })
-       .catch( err => {
-           res.status(500).send(err)
-       });
-   },
-   getFavorites: (req, res, next) => {
+    dbInstance
+      .delete_favorite([favorite_id])
+      .then(results => {
+        res.status(200).send('Favorite has been removed');
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  },
+  getFavorites: (req, res, next) => {
     const dbInstance = req.app.get('db');
 
     const { userEmail } = req.body;
 
-    dbInstance.get_user([userEmail])
-    .then( results => {
-        dbInstance.get_favorites([results[0].id])
-        .then( results => {
-            res.status(200).send(results)
-        })
-        .catch( err => {
+    dbInstance
+      .get_user([userEmail])
+      .then(results => {
+        dbInstance
+          .get_favorites([results[0].id])
+          .then(results => {
+            res.status(200).send(results);
+          })
+          .catch(err => {
             res.status(500).send(err);
-        });
-    })
-    .catch( err => {
+          });
+      })
+      .catch(err => {
         res.status(500).send(err);
-    });
-   }
-}
+      });
+  }
+};
