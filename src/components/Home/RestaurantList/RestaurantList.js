d383defb18e69b5a37cb0cd4ef6cc1fca197dd41
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-08-24 09:14:14 -0600

    working on filters

--- src/components/Home/RestaurantList/RestaurantList.js
+++ src/components/Home/RestaurantList/RestaurantList.js
@@ -4,7 +4,30 @@ import { getRestaurants } from "../../../actions/locationActions";
 import List from "./ListItem";
 
 class RestaurantList extends Component {
+	state = {
+		localRestaurants: [],
+		localRestaurantsToShow: 'all'
+	}
+
+	updateRestaurantToShow = s => {
+		this.setState({
+			localRestaurantsToShow: s
+		});
+	  };
+
+
 	render() {
+		let localRestaurants = [];
+			console.log(this.props.localRestaurants)
+		if (this.state.localRestaurantsToShow === "all") {
+			localRestaurants = this.props.localRestaurants;
+		  } else if (this.state.localRestaurantsToShow === 'rating'){
+			  localRestaurants = this.props.localRestaurants.filter(rating => rating >= 4.5)
+			  console.log(this.props.localRestaurants)
+		  } else if (this.state.localRestaurantsToShow === 'openNow'){
+			  localRestaurants = this.props.localRestaurants.openNow.filte
+		  }
+
 		const list = this.props.localRestaurants.map((e, i) => {
 			i = e.length - 1;
 			return (
@@ -21,6 +44,10 @@ class RestaurantList extends Component {
 
 		return (
 			<div className='restaurant-list'>
+			<div>
+				<button onClick={() => this.updateRestaurantToShow('all')} >Show all</button>
+				<button onClick={() => this.updateRestaurantToShow('rating')} >Rating 4.5+</button>
+			</div>
 				{console.log(this.props.localRestaurants)}
 				{list}
 			</div>
